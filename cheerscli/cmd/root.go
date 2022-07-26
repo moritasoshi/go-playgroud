733e5e4e47589b3de082ab2c2e968f539a8deb9c
morita <moritasoushi@gmail.com>
2022-07-26 23:42:18 +0900

    create cheerscli command

--- cheerscli/cmd/root.go
+++ cheerscli/cmd/root.go
@@ -5,12 +5,13 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +25,11 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		greetings := []string{"hola", "bonjour", "guten tag", "hello", "konnichiwa"}
+		rand.Seed(time.Now().Unix())
+		fmt.Printf("%s!", greetings[rand.Intn(len(greetings))])
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +52,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
